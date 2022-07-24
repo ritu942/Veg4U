67673fdc5c3f8fe7fe6931005f95159125d441c7
9GAG <sonalanand102@gmail.com>
2022-07-24 11:20:06 +0530

    seeders for product details added

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -17,19 +17,85 @@ class ProductSeeder extends Seeder
         //
         DB::table('products')->insert([
             [
-                'image' => '../public/images/products/strawberry.jpg',
-                'name'  => 'Strawberries',
-                'category_id' => 5,
+                'image' => '../public/images/products/anar_fruit.jpg',
+                'name'  => 'Pomegranate',
+                'category_id' => 1,
+                'weight' => 1,
+                'price' => 101
+            ],
+
+            [
+                'image' => '../public/images/products/mangos_fruit.jpg',
+                'name'  => 'Mangoes',
+                'category_id' => 1,
+                'weight' => 1,
+                'price' => 60
+            ],
+
+            [
+                'image' => '../public/images/products/oranges_fruit.jpg',
+                'name'  => 'Oranges',
+                'category_id' => 1,
+                'weight' => 1,
+                'price' => 40
+            ],
+
+            [
+                'image' => '../public/images/products/pineapples_fruit.jpg',
+                'name'  => 'Pineapples',
+                'category_id' => 1,
+                'weight' => 1,
+                'price' => 85
+            ],
+
+            [
+                'image' => '../public/images/products/strawberry_fruit.jpg',
+                'name'  => 'Strawberry',
+                'category_id' => 1,
                 'weight' => 1,
                 'price' => 201
             ],
-            // [
-            //     'image' => 'https://www.thespruce.com/thmb/kFaENoz7H7wEK-Ivc2OmPTGCWUs=/434x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/grow-vegetables-without-full-sun-4150681-06-4fcbc9e0f7694fbd96ff02cd231a5033.jpg',
-            //     'name'  => 'Cabbage',
-            //     'category_id' => 6,
-            //     'weight' => 1,
-            //     'price' => 21
-            // ]
+
+            [
+                'image' => '../public/images/products/beans_vege.jpg',
+                'name'  => 'Beans',
+                'category_id' => 2,
+                'weight' => 1,
+                'price' => 40
+            ],
+
+            [
+                'image' => '../public/images/products/cabbage_vege.jpg',
+                'name'  => 'Cabbage',
+                'category_id' => 2,
+                'weight' => 1,
+                'price' => 21
+            ],
+
+            [
+                'image' => '../public/images/products/potatos_vege.jpg',
+                'name'  => 'Potatoes',
+                'category_id' => 2,
+                'weight' => 1,
+                'price' => 21
+            ],
+
+            [
+                'image' => '../public/images/products/spnich_vege.jpg',
+                'name'  => 'Spnich',
+                'category_id' => 2,
+                'weight' => 1,
+                'price' => 21
+            ],
+
+            [
+                'image' => '../public/images/products/tomatos_vege.jpg',
+                'name'  => 'Tomatoes',
+                'category_id' => 2,
+                'weight' => 1,
+                'price' => 40
+            ],
+
         ]);
     }
 }
